b48434b79bd6c83eff10452494ed93b260f36681
user <bdcs008@naver.com>
2025-10-25 02:35:24 +0900

    [feat] views API(chat/views.py) URL 연결 #2

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:other_user_id>/', views.chat_room, name='chat_room'),
+
+    path('api/messages/<str:room_name>/', views.MessageHistoryView.as_view(), name='message-history-api'),
 ]
