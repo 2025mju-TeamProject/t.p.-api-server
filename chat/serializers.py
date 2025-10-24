8da23e2ba4313ff41a697c64c3b834f5f49f43ab
user <bdcs008@naver.com>
2025-10-25 01:32:50 +0900

    [feat] serializers.py 생성 (메시지 모델을 JSON으로 변환해주는 파일) #2

---
+++ chat/serializers.py
@@ -0,0 +1,11 @@
+# chat/serializers.py
+
+from rest_framework import serializers
+from .models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    # sender 필드를 기본 ID 대신 username으로 표시
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'content', 'timestamp']
