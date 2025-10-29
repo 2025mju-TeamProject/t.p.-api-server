ec6e63063760776d731653c1704d32b7f1549f40
user <bdcs008@naver.com>
2025-10-29 20:37:56 +0900

    [Refactor] asgi 리팩토링 #5

---
+++ config/asgi.py
@@ -0,0 +1,29 @@
+# TeamProject/TeamProject/asgi.py (최종본)
+
+import os
+from django.core.asgi import get_asgi_application
+
+# --- 채팅(Channels) 관련 임포트 ---
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # chat 앱의 routing.py를 불러옵니다.
+
+
+# --- ✨ settings.py 분리 관련 수정 사항 ---
+# TeamProject.settings가 아닌, TeamProject.settings.dev를 기본으로!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+
+
+# --- ✨ 채팅(Channels) 관련 라우팅 설정 ---
+#
+# 1. http:// 요청은 기존 Django 방식으로 (get_asgi_application() 사용)
+# 2. ws:// 요청은 Channels 방식으로 (AuthMiddlewareStack/URLRouter 사용)
+#
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
