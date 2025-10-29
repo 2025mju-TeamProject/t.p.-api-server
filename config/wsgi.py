1058bf14e80a34e84c371a3f9add84236521995c
user <bdcs008@naver.com>
2025-10-29 20:38:09 +0900

    [Refactor] wsgi 리팩토링 #5

---
+++ config/wsgi.py
@@ -0,0 +1,11 @@
+# TeamProject/TeamProject/wsgi.py (최종본)
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+# --- ✨ settings.py 분리 관련 수정 사항 ---
+# TeamProject.settings가 아닌, TeamProject.settings.dev를 기본으로!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+
+# --- 기존 WSGI 애플리케이션 로드 ---
+application = get_wsgi_application()
