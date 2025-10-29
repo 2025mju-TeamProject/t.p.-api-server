238ac91c73d9c0dc9e528fda0ad9dad5b12d16c4
user <bdcs008@naver.com>
2025-10-30 03:23:03 +0900

    [fix] 개발 설정 변경 및 테스트 페이지 수정 #2

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,16 +56,16 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'channels',
     'daphne',
+    'chat',
+    'api',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-    'api',
-    'chat',
 ]
 
 MIDDLEWARE = [
