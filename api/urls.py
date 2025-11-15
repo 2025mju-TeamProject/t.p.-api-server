33602044a278be082f3ca3c756c01542a37d8703
wlsdnjs0118 <bdcs008@naver.com>
2025-11-16 02:58:59 +0900

    [Fix] 로그인 할 때 Hash된 비밀번호 POST 못 하는 오류 해결 #11

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from . import views  # 현재 폴더(api)에 있는 views.py를 불러오기
 
 urlpatterns = [
-    # http://127.0.0.1:8000/api/profile/ 주소로 요청이 오면 views.py의 generate_profile 함수를 실행
-    path('profile/', views.generate_profile, name='generate_profile'),
-
+    # 프로필 조회(GET), 생성(POST), 수정(PATCH) API
+    path('profile/', views.ProfileView.as_view(), name='manage_profile'),
+    # 회원가입(POST) API
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
     # http://127.0.0.1:8000/api/report/ 주소로 요청이 오면 views.py의 generate_compatibility_report 함수를 실행
     path('report/', views.generate_compatibility_report, name='generate_compatibility_report'),
 
