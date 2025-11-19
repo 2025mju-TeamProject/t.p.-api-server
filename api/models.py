8cd53335f0c35de58bb5395c7147fab13ddab60d
wlsdnjs0118 <bdcs008@naver.com>
2025-11-20 04:20:36 +0900

    [Feat] 필드 추가 및 이미지 모델 생성 #14

--- api/models.py
+++ api/models.py
@@ -12,23 +12,53 @@ class Profile(models.Model):
     # User가 회원가입하면 그에 딸린 Profile 객체 1개 생성
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
 
-    # 1. 'generate_profile'이 사용하는 정보
-    nickname = models.CharField(max_length=50, blank=True, null=True)
+    # 1. 성별 선택
+    gender = models.CharField(max_length=10, blank=True, null=True)
+
+    # 2. 생년월일/태어난 시간, 분 선택
     year = models.IntegerField(blank=True, null=True)
     month = models.IntegerField(blank=True, null=True)
     day = models.IntegerField(blank=True, null=True)
     hour = models.IntegerField(blank=True, null=True)
     minute = models.IntegerField(blank=True, null=True)
-    gender = models.CharField(max_length=10, blank=True, null=True)
-    job = models.CharField(max_length=50, blank=True, null=True)
+    # '시간 모름' 경우
+    birth_time_unknown = models.BooleanField(default=False)
+
+    # 3. 관심사
     hobbies = models.JSONField(blank=True, null=True)  # 리스트는 JSONField로 저장
+
+    # 4. MBTI (선택)
     mbti = models.CharField(max_length=10, blank=True, null=True)
-    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    # 2. GPT가 생성하고, 사용자가 수정할 필드
+
+    # 5. 직업 (선택)
+    job = models.CharField(max_length=50, blank=True, null=True)
+
+    # 6. 지역 (선택)
+    # 시/도
+    location_city = models.CharField(max_length=50, blank=True, null=True)
+    # 시/군/구
+    location_district = models.CharField(max_length=50, blank=True, null=True)
+
+    # 7. 프로필 사진은 ProfileImage 모델에서 관리
+    # 8. AI 생성 텍스트
     profile_text = models.TextField(blank=True, null=True)
+
+    # 회원가입에 사용할 휴대폰 번호
+    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    # 1. 'generate_profile'이 사용하는 정보
+    nickname = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username}의 프로필'
+
+# 프로필 이미지 모델
+class ProfileImage(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='profile_images/') # 'images/profile_images/' 디렉에 저장됨
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.profile.user.username}의 사진 {self.id}"
 
 # User가 생성될 때 Profile도 자동으로 생성
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
