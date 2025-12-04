171e6282845e970ee088a74bb7cf78f87ffd56f5
wlsdnjs0118 <bdcs008@naver.com>
2025-12-05 02:52:35 +0900

    [Feat] Chat url 수정 #38

--- chat/urls.py
+++ chat/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/messages/<int:target_id>/', views.MessageHistoryView.as_view(), name='message-history-api'),
+    path('api/history-messages/<int:target_id>/', views.MessageHistoryView.as_view(), name='message-history-api'),
     path('api/block/<int:user_id_to_block>/', views.BlockUserView.as_view(), name='block-user-api'),
     path('api/suggestions/<int:target_id>/', views.ChatSuggestionView.as_view(), name='chat-suggestions-api'),
     path('api/rooms/', views.ChatRoomListView.as_view(), name='chat-room-list-api'),
-    path('api/message/<int:target_id>/', views.MessageSendView.as_view(), name='message-send')
+    path('api/send-messages/<int:target_id>/', views.MessageSendView.as_view(), name='message-send')
 ]
